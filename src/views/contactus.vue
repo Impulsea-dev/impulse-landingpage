139631bfbb25bb71fa1b059d8e462ee4a659b41c
dexstager <rxdraft@proton.me>
2023-05-04 07:38:40 -0600

    changes

--- src/views/contactus.vue
+++ src/views/contactus.vue
@@ -1,179 +1,318 @@
 <template>
-    <div class="flex items-center justify-center h-5/6">
+    <div>
+      <div class="relative items-center justify-center h-auto  dark:!bg-[#3C007C]  !p-12 w-[100vw] !-left-6 -top-6 ">
      
    
-  <card :className="'dark:!bg-[#000000]  lg:w-[80%]  mt-6 '">
-  <div class="grid grid-cols-1 lg:grid-cols-3 mt-3 ">
-      <div class="flex flex-col">
-        <h5 class="mb-6">
-      Send project details. Our team will contact you ASAP.
-
-    </h5>
-
-    <div class="space-y-4">
-<Textinput
-  label="Your Name"
-  name="v_Fullname"
-  type="text"
-  placeholder="Your Name"
-/>
-<Textinput
-  label="Email"
-  name="v_email"
-  type="email"
-  placeholder="Type your email"
-/>
-<Textarea label="Message" name="pn4" placeholder="Your message" />
-<Button text="Get in touch" btnClass="btn-primary block-btn"  style="background:linear-gradient(224.95deg, #a446f4 -1.95%, #4138f3 104.5%)"  />
-
-</div>
-
-
-      </div>
-      <div class="flex flex-col">
-        <img class="w-[636px] h-[507px] rounded-2xl" :src="contact" />
-<div class="w-[592px] h-[52px]">
-  <div class="flex flex-col justify-start items-start w-44 absolute left-[740px] top-[212px] gap-1">
-    <div class="flex justify-center items-center flex-grow-0 flex-shrink-0 relative gap-2">
-      <svg
-        width="16"
-        height="16"
-        viewBox="0 0 16 16"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-        class="flex-grow-0 flex-shrink-0 w-4 h-4"
-        preserveAspectRatio="xMidYMid meet"
-      >
-        <path d="M0 0H16V16H0V0Z" fill="white" fill-opacity="0.01"></path>
-        <path
-          d="M2.66699 11.9999V14.6666H13.3337V1.33325H2.66699V3.99992"
-          stroke="white"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-        ></path>
-        <path
-          d="M2 9.99994H3.33333"
-          stroke="white"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-        ></path>
-        <path
-          d="M2 7.99994H3.33333"
-          stroke="white"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-        ></path>
-        <path
-          d="M2 5.99994H3.33333"
-          stroke="white"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-        ></path>
-        <path
-          d="M8.00033 6.99992C8.73671 6.99992 9.33366 6.40296 9.33366 5.66659C9.33366 4.93021 8.73671 4.33325 8.00033 4.33325C7.26395 4.33325 6.66699 4.93021 6.66699 5.66659C6.66699 6.40296 7.26395 6.99992 8.00033 6.99992Z"
-          fill="#2F88FF"
-          stroke="white"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-        ></path>
-        <path
-          d="M10.6663 11.6666C10.6663 10.1938 9.47244 8.99994 7.99967 8.99994C6.52691 8.99994 5.33301 10.1938 5.33301 11.6666"
-          stroke="white"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-        ></path>
-      </svg>
-      <p class="flex-grow-0 flex-shrink-0 text-base font-bold text-left text-white">Address</p>
-    </div>
-    <p class="flex-grow-0 flex-shrink-0 text-base font-medium text-left text-white/70">
-      Company address 2340
-    </p>
-  </div>
-  <div class="flex flex-col justify-start items-start w-44 absolute left-[948px] top-[212px] gap-1">
-    <div class="flex justify-center items-center flex-grow-0 flex-shrink-0 relative gap-2">
-      <svg
-        width="16"
-        height="16"
-        viewBox="0 0 16 16"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-        class="flex-grow-0 flex-shrink-0 w-4 h-4"
-        preserveAspectRatio="xMidYMid meet"
-      >
-        <path d="M0 0H16V16H0V0Z" fill="white" fill-opacity="0.01"></path>
-        <path d="M0 0H16V16H0V0Z" fill="white" fill-opacity="0.01"></path>
-        <path
-          d="M2.66602 10H13.3327V14C13.3327 14.3682 13.0342 14.6667 12.666 14.6667H3.33268C2.96449 14.6667 2.66602 14.3682 2.66602 14V10Z"
-          stroke="white"
-          stroke-linejoin="round"
-        ></path>
-        <path
-          d="M13.3327 9.99998V1.99998C13.3327 1.63179 13.0342 1.33331 12.666 1.33331H3.33268C2.96449 1.33331 2.66602 1.63179 2.66602 1.99998V9.99998"
-          stroke="white"
-          stroke-linejoin="round"
-        ></path>
-        <path d="M7.33301 12.3333H8.66634" stroke="white" stroke-linecap="round"></path>
-      </svg>
-      <p class="flex-grow-0 flex-shrink-0 text-base font-bold text-left text-white">Phone number</p>
-    </div>
-    <p class="flex-grow-0 flex-shrink-0 text-base font-medium text-left text-white/70">
-      +491234002
-    </p>
-  </div>
-  <div
-    class="flex flex-col justify-start items-start w-44 absolute left-[1156px] top-[212px] gap-1"
-  >
-    <div class="flex justify-center items-center flex-grow-0 flex-shrink-0 relative gap-2">
-      <svg
-        width="16"
-        height="16"
-        viewBox="0 0 16 16"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-        class="flex-grow-0 flex-shrink-0 w-4 h-4"
-        preserveAspectRatio="xMidYMid meet"
-      >
-        <path d="M0 0H16V16H0V0Z" fill="white" fill-opacity="0.01"></path>
-        <path
-          d="M1.33301 13H14.6663V8V3H7.99967H1.33301V8V13Z"
-          stroke="white"
-          stroke-linejoin="round"
-        ></path>
-        <path
-          d="M1.33301 3L7.99967 8L14.6663 3"
-          stroke="white"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-        ></path>
-        <path
-          d="M7.99967 3H1.33301V8"
-          stroke="white"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-        ></path>
-        <path
-          d="M14.6667 8V3H8"
-          stroke="white"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-        ></path>
-      </svg>
-      <p class="flex-grow-0 flex-shrink-0 text-base font-bold text-left text-white">Email</p>
-    </div>
-    <p class="flex-grow-0 flex-shrink-0 text-base font-medium text-left text-white/70">
-      yourcompany@email
-    </p>
-  </div>
-</div>
-      </div>
-
-  </div>
-
-  </card> 
-
-
-
- 
+     <card :className="'dark:!bg-[#000000]  lg:w-[80%]  mt-6 relative ml-auto mr-auto'">
+     <div class="grid grid-cols-1 lg:grid-cols-2 mt-3 ">
+         <div class="flex flex-col">
+           <h5 class="mb-6">
+         Send project details. Our team will contact you ASAP.
+   
+       </h5>
+   
+       <div class="space-y-4">
+   <Textinput
+     label="Your Name"
+     name="v_Fullname"
+     type="text"
+     placeholder="Your Name"
+   />
+   <Textinput
+     label="Email"
+     name="v_email"
+     type="email"
+     placeholder="Type your email"
+   />
+   <Textarea label="Message" name="pn4" placeholder="Your message" />
+   <Button text="Get in touch" btnClass="btn-primary block-btn"  style="background:linear-gradient(224.95deg, #a446f4 -1.95%, #4138f3 104.5%)"  />
+   
+   </div>
+   
+   
+         </div>
+         <div class="hidden flex-col lg:flex lg:ml-10  ">
+          
+   <div class=" grid grid-cols-3">
+     <div class="flex-col  w-44   gap-1 relative">
+       <div class="flex justify-start items-center flex-grow-0 flex-shrink-0  gap-2">
+         <svg
+           width="16"
+           height="16"
+           viewBox="0 0 16 16"
+           fill="none"
+           xmlns="http://www.w3.org/2000/svg"
+           class="flex-grow-0 flex-shrink-0 w-4 h-4"
+           preserveAspectRatio="xMidYMid meet"
+         >
+           <path d="M0 0H16V16H0V0Z" fill="white" fill-opacity="0.01"></path>
+           <path
+             d="M2.66699 11.9999V14.6666H13.3337V1.33325H2.66699V3.99992"
+             stroke="white"
+             stroke-linecap="round"
+             stroke-linejoin="round"
+           ></path>
+           <path
+             d="M2 9.99994H3.33333"
+             stroke="white"
+             stroke-linecap="round"
+             stroke-linejoin="round"
+           ></path>
+           <path
+             d="M2 7.99994H3.33333"
+             stroke="white"
+             stroke-linecap="round"
+             stroke-linejoin="round"
+           ></path>
+           <path
+             d="M2 5.99994H3.33333"
+             stroke="white"
+             stroke-linecap="round"
+             stroke-linejoin="round"
+           ></path>
+           <path
+             d="M8.00033 6.99992C8.73671 6.99992 9.33366 6.40296 9.33366 5.66659C9.33366 4.93021 8.73671 4.33325 8.00033 4.33325C7.26395 4.33325 6.66699 4.93021 6.66699 5.66659C6.66699 6.40296 7.26395 6.99992 8.00033 6.99992Z"
+             fill="#2F88FF"
+             stroke="white"
+             stroke-linecap="round"
+             stroke-linejoin="round"
+           ></path>
+           <path
+             d="M10.6663 11.6666C10.6663 10.1938 9.47244 8.99994 7.99967 8.99994C6.52691 8.99994 5.33301 10.1938 5.33301 11.6666"
+             stroke="white"
+             stroke-linecap="round"
+             stroke-linejoin="round"
+           ></path>
+         </svg>
+         <p class="flex-grow-0 flex-shrink-0 text-base font-bold text-left text-white">Address</p>
+       </div>
+       <p class="flex-grow-0 flex-shrink-0 text-base font-medium text-left text-white/70">
+         Company address 2340
+       </p>
+     </div>
+     <div class="flex-col  w-44   gap-1 relative">
+       <div class="flex justify-start items-center flex-grow-0 flex-shrink-0  gap-2">
+         <svg
+           width="16"
+           height="16"
+           viewBox="0 0 16 16"
+           fill="none"
+           xmlns="http://www.w3.org/2000/svg"
+           class="flex-grow-0 flex-shrink-0 w-4 h-4"
+           preserveAspectRatio="xMidYMid meet"
+         >
+           <path d="M0 0H16V16H0V0Z" fill="white" fill-opacity="0.01"></path>
+           <path d="M0 0H16V16H0V0Z" fill="white" fill-opacity="0.01"></path>
+           <path
+             d="M2.66602 10H13.3327V14C13.3327 14.3682 13.0342 14.6667 12.666 14.6667H3.33268C2.96449 14.6667 2.66602 14.3682 2.66602 14V10Z"
+             stroke="white"
+             stroke-linejoin="round"
+           ></path>
+           <path
+             d="M13.3327 9.99998V1.99998C13.3327 1.63179 13.0342 1.33331 12.666 1.33331H3.33268C2.96449 1.33331 2.66602 1.63179 2.66602 1.99998V9.99998"
+             stroke="white"
+             stroke-linejoin="round"
+           ></path>
+           <path d="M7.33301 12.3333H8.66634" stroke="white" stroke-linecap="round"></path>
+         </svg>
+         <p class="flex-grow-0 flex-shrink-0 text-base font-bold text-left text-white">Phone number</p>
+       </div>
+       <p class="flex-grow-0 flex-shrink-0 text-base font-medium text-left text-white/70">
+         +491234002
+       </p>
+     </div>
+     <div
+       class="flex-col  w-44   gap-1 relative"
+     >
+       <div class="flex justify-start items-center flex-grow-0 flex-shrink-0  gap-2">
+         <svg
+           width="16"
+           height="16"
+           viewBox="0 0 16 16"
+           fill="none"
+           xmlns="http://www.w3.org/2000/svg"
+           class="flex-grow-0 flex-shrink-0 w-4 h-4"
+           preserveAspectRatio="xMidYMid meet"
+         >
+           <path d="M0 0H16V16H0V0Z" fill="white" fill-opacity="0.01"></path>
+           <path
+             d="M1.33301 13H14.6663V8V3H7.99967H1.33301V8V13Z"
+             stroke="white"
+             stroke-linejoin="round"
+           ></path>
+           <path
+             d="M1.33301 3L7.99967 8L14.6663 3"
+             stroke="white"
+             stroke-linecap="round"
+             stroke-linejoin="round"
+           ></path>
+           <path
+             d="M7.99967 3H1.33301V8"
+             stroke="white"
+             stroke-linecap="round"
+             stroke-linejoin="round"
+           ></path>
+           <path
+             d="M14.6667 8V3H8"
+             stroke="white"
+             stroke-linecap="round"
+             stroke-linejoin="round"
+           ></path>
+         </svg>
+         <p class="flex-grow-0 flex-shrink-0 text-base font-bold text-left text-white">Email</p>
+       </div>
+       <p class="flex-grow-0 flex-shrink-0 text-base font-medium text-left text-white/70">
+         yourcompany@email
+       </p>
+     </div>
+   </div>
+    <img class="w-[636px] h-auto rounded-2xl" :src="contact" />
+         </div>
+   
+     </div>
+   
+     </card> 
+   
+   
+   
+    
+       </div>
+       <div class="flex flex-col lg:hidden  ml-auto  justify-center ">
+          
+          <div class=" grid grid-cols-1">
+            <div class="flex-col   w-44   gap-1 relative">
+              <div class="flex justify-center items-center   gap-2">
+                <svg
+                  width="16"
+                  height="16"
+                  viewBox="0 0 16 16"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="flex-grow-0 flex-shrink-0 w-4 h-4"
+                  preserveAspectRatio="xMidYMid meet"
+                >
+                  <path d="M0 0H16V16H0V0Z" fill="white" fill-opacity="0.01"></path>
+                  <path
+                    d="M2.66699 11.9999V14.6666H13.3337V1.33325H2.66699V3.99992"
+                    stroke="white"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  ></path>
+                  <path
+                    d="M2 9.99994H3.33333"
+                    stroke="white"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  ></path>
+                  <path
+                    d="M2 7.99994H3.33333"
+                    stroke="white"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  ></path>
+                  <path
+                    d="M2 5.99994H3.33333"
+                    stroke="white"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  ></path>
+                  <path
+                    d="M8.00033 6.99992C8.73671 6.99992 9.33366 6.40296 9.33366 5.66659C9.33366 4.93021 8.73671 4.33325 8.00033 4.33325C7.26395 4.33325 6.66699 4.93021 6.66699 5.66659C6.66699 6.40296 7.26395 6.99992 8.00033 6.99992Z"
+                    fill="#2F88FF"
+                    stroke="white"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  ></path>
+                  <path
+                    d="M10.6663 11.6666C10.6663 10.1938 9.47244 8.99994 7.99967 8.99994C6.52691 8.99994 5.33301 10.1938 5.33301 11.6666"
+                    stroke="white"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  ></path>
+                </svg>
+                <p class="flex-grow-0 flex-shrink-0 text-base font-bold text-left text-white">Address</p>
+              </div>
+              <p class="text-base font-medium text-center text-white/70">
+                Company address 2340
+              </p>
+            </div>
+            <div class="flex-col  w-44   gap-1 relative">
+              <div class="flex justify-cemter items-center gap-2">
+                <svg
+                  width="16"
+                  height="16"
+                  viewBox="0 0 16 16"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="flex-grow-0 flex-shrink-0 w-4 h-4"
+                  preserveAspectRatio="xMidYMid meet"
+                >
+                  <path d="M0 0H16V16H0V0Z" fill="white" fill-opacity="0.01"></path>
+                  <path d="M0 0H16V16H0V0Z" fill="white" fill-opacity="0.01"></path>
+                  <path
+                    d="M2.66602 10H13.3327V14C13.3327 14.3682 13.0342 14.6667 12.666 14.6667H3.33268C2.96449 14.6667 2.66602 14.3682 2.66602 14V10Z"
+                    stroke="white"
+                    stroke-linejoin="round"
+                  ></path>
+                  <path
+                    d="M13.3327 9.99998V1.99998C13.3327 1.63179 13.0342 1.33331 12.666 1.33331H3.33268C2.96449 1.33331 2.66602 1.63179 2.66602 1.99998V9.99998"
+                    stroke="white"
+                    stroke-linejoin="round"
+                  ></path>
+                  <path d="M7.33301 12.3333H8.66634" stroke="white" stroke-linecap="round"></path>
+                </svg>
+                <p class="text-base font-bold text-left text-white">Phone number</p>
+              </div>
+              <p class="flex flex-col justify-center text-base font-medium text-center text-white/70">
+                +491234002
+              </p>
+            </div>
+            <div
+              class="flex-col  w-44   gap-1 relative"
+            >
+              <div class="flex justify-center items-center flex-grow-0 flex-shrink-0  gap-2">
+                <svg
+                  width="16"
+                  height="16"
+                  viewBox="0 0 16 16"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="flex-grow-0 flex-shrink-0 w-4 h-4"
+                  preserveAspectRatio="xMidYMid meet"
+                >
+                  <path d="M0 0H16V16H0V0Z" fill="white" fill-opacity="0.01"></path>
+                  <path
+                    d="M1.33301 13H14.6663V8V3H7.99967H1.33301V8V13Z"
+                    stroke="white"
+                    stroke-linejoin="round"
+                  ></path>
+                  <path
+                    d="M1.33301 3L7.99967 8L14.6663 3"
+                    stroke="white"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  ></path>
+                  <path
+                    d="M7.99967 3H1.33301V8"
+                    stroke="white"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  ></path>
+                  <path
+                    d="M14.6667 8V3H8"
+                    stroke="white"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  ></path>
+                </svg>
+                <p class="flex-grow-0 flex-shrink-0 text-base font-bold text-left text-white">Email</p>
+              </div>
+              <p class="flex-grow-0 flex-shrink-0 text-base font-medium text-left text-white/70">
+                yourcompany@email
+              </p>
+            </div>
+          </div> 
+                </div>
     </div>
   </template>
   <script>
@@ -191,8 +330,9 @@
   };
   </script>
   <style>
-   body.dark {
-    background-color: #3C007C;
+  .changeColor {
+    background-color: #3C007C !important;
+    
    }
   </style>
   
