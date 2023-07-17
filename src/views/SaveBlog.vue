377266545e93ab4569ebc0df9406421027c3e449
David Alvarado <davidalvarado470@gmail.com>
2023-07-17 15:36:51 -0600

    cambios

--- src/views/SaveBlog.vue
+++ src/views/SaveBlog.vue
@@ -65,14 +65,12 @@ export default defineComponent({
         const { value: title, errorMessage: titleError } = useField("title");
         const { value: description, errorMessage: descriptionError } = useField("description");
         const getImage = (img) => {
-            
-
             const reader = new FileReader();
-    reader.readAsDataURL(img);
-    reader.onload = () =>{image.value=reader.result; console.log(image.value)} ;
+            reader.readAsDataURL(img);
+            reader.onload = () => { image.value = reader.result; console.log(image.value) };
         }
-
-        const saveBlog = handleSubmit(async() => {
+        
+        const saveBlog = handleSubmit(async () => {
             loading.value = true
             if (editor.value.getText().trim() == '' || !image.value) {
                 swal({
@@ -87,20 +85,20 @@ export default defineComponent({
                 loading.value = false
             }
             else {
-                const info={
+                const info = {
                     title: title.value,
                     description: description.value,
                     content: editor.value.getHTML(),
                     header: image.value,
-                    authorization:{"authorization":"v2.local.GVeOrs018trhRPqNO22QvpFcDM7WnG9nkuhWa8JeFowL5zCwffe61jg7zBvyyz9DEJ5F07ecNd7qrKMLY0YML1NdBqmu5TIw2nOIQM5BVymewVsNErPVNSoA_TOIA2ORc95Qp0RXU6fBuQ-OtBdQkZ7cI9In15UsD2IEj6x1QBUkT1Sd9bP0hETc1ZGsgCrfhrXKarAv-FgOiVv4pgVZVklS2F89M7iB4NpW9jE4kKru4zDveui7GSPX75vgwihiHldtM2neafLIBYRYTgnKim9GtZg0dMWGA4w4C4jpau_jil2jKNv8nUT15KM1eOgDrvSS6m8bjbH9c4LidtT1tSWwiF3JiPtxk5-QaKUrQb1nB5mZKd_jhoBkshMbRGFqesmoNAeixeosjIiG.bnVsbA"}
+                    authorization: { "authorization": "v2.local.GVeOrs018trhRPqNO22QvpFcDM7WnG9nkuhWa8JeFowL5zCwffe61jg7zBvyyz9DEJ5F07ecNd7qrKMLY0YML1NdBqmu5TIw2nOIQM5BVymewVsNErPVNSoA_TOIA2ORc95Qp0RXU6fBuQ-OtBdQkZ7cI9In15UsD2IEj6x1QBUkT1Sd9bP0hETc1ZGsgCrfhrXKarAv-FgOiVv4pgVZVklS2F89M7iB4NpW9jE4kKru4zDveui7GSPX75vgwihiHldtM2neafLIBYRYTgnKim9GtZg0dMWGA4w4C4jpau_jil2jKNv8nUT15KM1eOgDrvSS6m8bjbH9c4LidtT1tSWwiF3JiPtxk5-QaKUrQb1nB5mZKd_jhoBkshMbRGFqesmoNAeixeosjIiG.bnVsbA" }
                 }
-                
-                const saveb= await BlogServices.saveBlog(info)
+
+                const saveb = await BlogServices.saveBlog(info)
                 console.log(info);
                 swal({
                     title: 'Great!',
                     text: 'Create Successfully',
-                    background:'#000',
+                    background: '#000',
                     icon: 'success',
                     showCancelButton: false,
                     confirmButtonColor: "#5cb85c",
