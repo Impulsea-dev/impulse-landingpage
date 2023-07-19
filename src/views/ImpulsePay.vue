b0c06447d03d3162bee4eb30cfad723ab6033dda
David Alvarado <davidalvarado470@gmail.com>
2023-07-19 14:50:08 -0600

    cambios

--- src/views/ImpulsePay.vue
+++ src/views/ImpulsePay.vue
@@ -1,11 +1,12 @@
 <template>
     <div class="!remove-padding-all">
-        <div class="relative w-full min-h-[600px] 2xl:min-h-[700px] bg-[#9898D1]">
-            <div class="absolute inset-0">
-                <div class="grid grid-cols-1 md:grid-cols-2">
+        <div class="relative w-full min-h-[400px] md:min-h-[600px] 2xl:min-h-[700px] bg-[#9898D1]">
+            <div class="absolute inset-0  mt-10">
+                <div class="hidden md:flex">
+                    <div class="grid grid-cols-1 md:grid-cols-2">
                     <div class="flex min-h-full flex-col pl-32 items-start justify-center w-full gap-4">
-                        <div class="md:text-6xl text-3xl font-bold text-white font-Monda">Impulse Pay</div>
-                        <div class="text-3xl text-white font-bold font-Monda">The Future of Payments</div>
+                        <div class="text-3xl md:text-6xl font-bold text-white font-Monda">Impulse Pay</div>
+                        <div class="text-2xl md:text-3xl text-white font-bold font-Monda">The Future of Payments</div>
                         <p class="text-base text-white font-Monda">In a world where seamless transactions and customer
                             satisfaction reign supreme,
                             Impulse Pay is the perfect solution for telecommunication companies. </p>
@@ -16,19 +17,30 @@
                             class="object-contain w-[450px] h-[450px] 2xl:w-[521px] 2xl:h-[521px] animate-pulse" />
                     </div>
                 </div>
+                </div>
+
+                <div class="flex md:hidden">
+                    <div class="flex flex-col text-center gap-2 mt-10">
+                        <div class="text-2xl font-bold text-white font-Monda">Impulse Pay</div>
+                        <div class="text-xl text-white font-bold font-Monda">The Future of Payments</div>
+                        <p class="text-sm text-white font-Monda">In a world where seamless transactions and customer
+                            satisfaction reign supreme,
+                            Impulse Pay is the perfect solution for telecommunication companies. </p>
+                    </div>
+                </div>
             </div>
         </div>
 
         <div class="mt-20 mb-20">
             <div class="flex flex-col items-center gap-2">
                 <div class="text-xl text-white font-bold font-Monda">Some of the best features</div>
-                <div class="text-base text-white font-Monda w-[550px]">Impulse Pay is a pre-built customer billing portal
+                <div class="text-base text-white font-Monda w-60 md:w-[550px]">Impulse Pay is a pre-built customer billing portal
                     that makes it easy for your
                     customers to settle their bills online any time, anywhere.</div>
             </div>
 
-            <div class="flex flex-row gap-32 justify-center mt-20">
-                <div class="flex flex-col gap-4 hover:scale-105">
+            <div class="flex flex-col md:flex-row gap-10 md:gap-32 justify-center mt-20">
+                <div class="flex flex-col gap-4 hover:scale-105 items-center">
                     <div
                         class="flex justify-center items-center w-[135px] h-[135px] rounded-[13.5px] bg-[#111315] border-[2.7px] border-none">
                         <img src="@/assets/images/bank.png" />
@@ -38,7 +50,7 @@
                     </div>
                 </div>
 
-                <div class="flex flex-col gap-4 hover:scale-105">
+                <div class="flex flex-col gap-4 hover:scale-105 items-center">
                     <div
                         class="flex justify-center items-center w-[135px] h-[135px] rounded-[13.5px] bg-[#111315] border-[2.7px] border-none">
                         <img src="@/assets/images/phone.png" />
@@ -48,7 +60,7 @@
                     </div>
                 </div>
 
-                <div class="flex flex-col gap-4 hover:scale-105">
+                <div class="flex flex-col gap-4 hover:scale-105 items-center">
                     <div
                         class="flex justify-center items-center w-[135px] h-[135px] rounded-[13.5px] bg-[#111315] border-[2.7px] border-none">
                         <img src="@/assets/images/clock.png" />
@@ -61,8 +73,9 @@
             </div>
         </div>
 
-        <div class="relative w-full min-h-[600px] 2xl:min-h-[700px] bg-[#9898D1]">
+        <div class="relative w-full min-h-[450px] md:min-h-[600px] 2xl:min-h-[700px] bg-[#9898D1]">
             <div class="absolute inset-0">
+             <div class="hidden md:flex">
                 <div class="grid grid-cols-1 md:grid-cols-2">
                     <div class="flex min-h-full flex-col pl-32 items-start justify-center w-full gap-4">
                         <div class="text-3xl text-white font-bold font-Monda">easy ,fast & one click payment</div>
@@ -82,18 +95,33 @@
                             class="object-contain w-[450px] h-[450px] 2xl:w-[521px] 2xl:h-[521px] " />
                     </div>
                 </div>
+             </div>
+             <div class="flex md:hidden">
+                <div class="flex flex-col text-center gap-2 mt-20">
+                    <div class="text-xl text-white font-bold font-Monda">easy ,fast & one click payment</div>
+                        <p class="text-sm text-white font-Monda">Our automated system allows customers to set up auto pay.
+                        </p>
+                        <p class="text-sm text-white font-Monda">Impulse Pay's automated system makes it easy for your
+                            customers to set up auto pay.</p>
+                        <p class="text-sm text-white font-Monda">They can simply log in to their account and pay their
+                            bill with a few clicks of a button.</p>
+                        <p class="text-sm text-white font-Monda">With Impulse Pay, your customers can seamlessly upgrade
+                            their packages, explore new offers, and take advantage of exciting promotions. </p>
+                    </div>
+             </div>
             </div>
         </div>
 
         <div class="mt-20">
-            <div class="flex flex-col items-center gap-2">
-                <div class="text-2xl text-white font-bold font-Monda">Goodbye to long queues and cumbersome processes</div>
-                <div class="text-base text-white font-Monda">With Impulse Pay, your customers can say goodbye
+            <div class="flex flex-col items-center text-center gap-2">
+                <div class="text-lg md:text-2xl text-white font-bold font-Monda">Goodbye to long queues and cumbersome processes</div>
+                <div class="text-sm text-white font-Monda">With Impulse Pay, your customers can say goodbye
                     to long queues and cumbersome processes.</div>
             </div>
 
-            <div class="relative w-full min-h-[450px] 2xl:min-h-[500px] bg-[#9898D1] mt-20">
+            <div class="relative w-full min-h-[300px] md:min-h-[450px] 2xl:min-h-[500px] bg-[#9898D1] mt-20">
                 <div class="absolute inset-0">
+                   <div class="hidden md:flex">
                     <div class="grid grid-cols-1 md:grid-cols-3 mt-20">
                         <div class="hidden md:flex justify-center items-center">
                             <img src="@/assets/images/bro.png" alt="Avatar" class="object-contain" />
@@ -104,6 +132,14 @@
                                 trust. I still learn that over and over again</p>
                         </div>
                     </div>
+                   </div>
+                   <div class="flex md:hidden mt-20">
+                    <div class="text-center">
+                        <div class="text-xl text-white font-bold font-Monda">Your money is 100% safe with us </div>
+                            <p class="text-sm text-white font-Monda">Sometimes you don’t know who you can and cannot
+                                trust. I still learn that over and over again</p>
+                    </div>
+                   </div>
                 </div>
             </div>
 
