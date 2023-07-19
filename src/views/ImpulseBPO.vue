d2f333e5ebe6ae31a0533b1faac398f85feb96e1
dexstager <rxdraft@proton.me>
2023-07-19 11:04:06 -0600

    changes

--- src/views/ImpulseBPO.vue
+++ src/views/ImpulseBPO.vue
@@ -1,7 +1,14 @@
 <template>
-<div>
+<div class="">
+<!-- Start --> 
+    <div class="!remove-padding-all relative w-full min-h-screen">
+  <img src="@/assets/images/bpostart.webp" alt="Avatar" class="object-cover w-full h-full" />
+  <div class="absolute bottom-0 inset-x-0 bg-blue-400 text-white text-xs text-left leading-4">
+    This is a text
+  </div> 
+</div>
 
-    
+
 </div>
 
 </template>
