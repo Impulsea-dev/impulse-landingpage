1a614bad013fb132205d31e176fd58d47d407c45
Ethan Diaz <diazethan@hotmail.com>
2024-05-15 09:20:29 -0600

    c

--- src/views/aboutus.vue
+++ src/views/aboutus.vue
@@ -35,8 +35,9 @@
         </div>
         </div>       
       </div> -->
-      <div class="flex justify-center md:items-center flex-col">
-        <Button text="Our Executive Team" btnClass="btn-primary mb-6"  style="background:#086C88;border-radius: 32px;"  />
+      <div class="bg-[#111315] w-full h-auto items-center justify-center mt-4 mb-10 rounded-lg p-2">
+      <div class="flex justify-start md:items-start flex-col" >
+        <Button text="Our Executive Team" btnClass="btn-primary mt-2"  style="background:#086C88;border-radius: 32px;"  />
         <!-- <div class="text-lg md:text-5xl lg:text-5xl text-white mb-4">
           Our Executive Team
         </div>  -->
@@ -70,7 +71,7 @@
 
       </div>
       </div>
-     
+     </div>
 
       <Button text="About Us" btnClass="btn-primary mb-6"  style="background:#086C88;border-radius: 32px;"  />
       <div class="grid lg:grid-cols-2 md:grid-cols-2 grid-cols-1 md:gap-6 md:space-y-0 space-x-14 mb-12">
