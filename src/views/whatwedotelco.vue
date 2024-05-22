510f9da222cf2826cd2ea858ef02b71deaffdcf3
Ethan Diaz <diazethan@hotmail.com>
2024-05-22 14:25:29 -0600

    changes

--- src/views/whatwedotelco.vue
+++ src/views/whatwedotelco.vue
@@ -2,7 +2,7 @@
   <div>
     <div class="grid lg:grid-cols-2 md:grid-cols-2 grid-cols-1 md:gap-6 md:space-y-0 space-x-28 mb-12">
       <div class="text-xl md:text-2xl lg:text-6xl lg:w-[875px] text-white mb-4 ml-4 font- animate-pulse">
-        Specialized telecom software delivered with deep industry experience
+        {{ $t('whatWeDoTitle') }}
       </div>
     </div>
     <!-- <div class="relative dark:bg-[#000000]  mt-2">
@@ -19,15 +19,15 @@
     </div> -->
 
     <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 gap-6">
-        <FlipCard :bgc="flipcard.background" :img="flipcard.img" :title="flipcard.title" 
-        :url="flipcard.url" :description="flipcard.description" v-for="(flipcard,i) in flipcards" :key="i"   /> 
+        <FlipCard :bgc="flipcard.background" :img="flipcard.img" :title=$t(flipcard.title)
+        :url="flipcard.url" :description=$t(flipcard.description) v-for="(flipcard,i) in flipcards" :key="i"   /> 
     </div>
 
     <div class="text-white text-center text-3xl mb-3 mt-40">
-      <span>Let’s Work Together </span>
+      <span>{{ $t('indexLetsWorkTogether') }}</span>
     </div>
     <div class="flex justify-center items-center gap-6 mb-6">
-      <Button text="Contact Us" btnClass="btn-primary "
+      <Button :text="$t('indexContactUs')" btnClass="btn-primary "
         style="background:linear-gradient(224.95deg, #a446f4 -1.95%, #4138f3 104.5%)" @click="btnContackUs" />
     </div>
   </div>
@@ -49,6 +49,7 @@ import bposerv from "@/assets/images/bpo.png"
 import productdev from "@/assets/images/desinganddev.png"
 import leads from "@/assets/images/impulseleads.png"
 import pay from "@/assets/images/impulsepay.png"
+import { useI18n } from 'vue-i18n'
 export default {
   components: {
     CardCustom,
@@ -65,45 +66,46 @@ export default {
   },
 
   setup(){
+    const { t } = useI18n()
       const flipcards = ref([
         {
           title:'Telemetrics',
-          description:'Advanced analytics for telecoms. Enhance visibility, simplify management, and optimize operations with our intelligent solution.',
+          description:"whatWeDoDescription-1",
           img:telemetrics,
           background:'bg-bg-btnp',
           url:'/telemetrics'
         },
         {
           title:'Impulse Bot',
-          description:'Improve Customer Satisfaction, Boost Sales, and Minimize Churn with Impulse Bot—an innovative AI and ML-powered chatbot designed to assist your customers round the clock.',
+          description:"whatWeDoDescription-2",
           img:boost,
           background:'bg-boost',
           url:'/impulsebot'
         },
         {
-          title:'BPO Services',
-          description:'Our BPO services include sales, support, and outbound services to streamline your business operations and enhance customer satisfaction.',
+          title:"indexOurSolutionsTitle-6",
+          description:"whatWeDoDescription-3",
           img:bposerv,
           background:'bg-bpo',
           url:'/bpo'
         },
         {
-          title:'Product Design and Development',
-          description:'Custom web solutions, billing systems, API development, and CRM integrations for telecommunications. Tailored to your unique needs, scalable solutions that drive growth and enhance customer satisfaction',
+          title:"whatWeDoProductDesignTitle",
+          description:"whatWeDoDescription-4",
           img:productdev,
           background:'bg-desingdev',
           url:'/product'
         },
         {
           title:'Impulse Leads',
-          description:'Empower your B2B endeavors with the crucial data and software to effortlessly connect and seal deals with your most valuable buyers, all conveniently integrated in one piace.',
+          description:"whatWeDoDescription-5",
           img:leads,
           background:'bg-imleads',
           url:'/impulse-lead'
         },
         {
           title:'Impulse Pay',
-          description:'Streamline your billing processes, enhance customer satisfaction, and simplify payments with Impulse Pays user-friendly portal. Say goodbye to long queues and complex procedures. Upgrade your telecom services effortlessly and experience the power of automation.',
+          description:"whatWeDoDescription-6",
           img:pay,
           background:'bg-impay',
           url:'/impulse-pay'
@@ -111,7 +113,7 @@ export default {
       ])
 
       return{
-        flipcards
+        flipcards,t
       }
   },
   methods: {
