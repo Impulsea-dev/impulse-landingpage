f350999dfab80a2dc10ab62a8c736ef9b860f52d
dexstager <rxdraft@proton.me>
2024-05-23 14:51:12 -0600

    "Translated website development page content to use i18n keys"

--- src/views/websitedevelopment.vue
+++ src/views/websitedevelopment.vue
@@ -5,13 +5,10 @@
                 <div class="grid grid-cols-1 xl:grid-cols-2 justify-between px-8 md:px-10 gap-4">
                     <div class="font-bold font-Monda flex flex-row gap-1">
                         <h1 class="!text-white text-2xl md:text-5xl">Impulse </h1>
-                        <h1 class="!text-[#04c5fe] text-2xl md:text-5xl">Website Design</h1>
+                        <h1 class="!text-[#04c5fe] text-2xl md:text-5xl">{{ $t('webDevelopmentInfo.impulseWebsiteDesign.title2') }}</h1>
                     </div>
                     <div class="font-Monda">
-                        <p class="!text-white">Impulse focuses on digital marketing for telecom companies. We build
-                            websites,
-                            mobile
-                            apps, and manage marketing to help telecoms & communication service providers achieve growth.
+                        <p class="!text-white"> {{ $t('webDevelopmentInfo.impulseWebsiteDesign.description') }} 
                         </p>
                     </div>
                 </div>
@@ -52,10 +49,9 @@
         </div>
         <div class="flex flex-col md:flex-row justify-between items-center gap-10 lg:mx-40 xl:mx-60 mt-32">
             <div class="flex flex-col md:w-[700px] animated-container opacity-0 font-Monda">
-                <h1 class="text-2xl md:text-4xl font-bold mb-4">Websites for Lead Generation</h1>
+                <h1 class="text-2xl md:text-4xl font-bold mb-4">{{ $t('webDevelopmentInfo.websiteForLeadGeneration.title') }}</h1>
                 <p>
-                    Telecom companies need websites that attract leads, customers, partners and advocates.
-                    We build websites that make publishing easy, are highly responsive, dynamic and optimized to convert.
+                    {{ $t('webDevelopmentInfo.websiteForLeadGeneration.description') }}
                 </p>
             </div>
             <div>
@@ -66,12 +62,9 @@
 
         <div class="flex flex-col md:flex-row-reverse justify-between items-center gap-10 lg:mx-40 xl:mx-60 mt-32">
             <div class="flex flex-col md:w-[700px] animated-container opacity-0 font-Monda">
-                <h1 class="text-2xl md:text-4xl font-bold mb-4">Responsive, Integrated, Optimized</h1>
+                <h1 class="text-2xl md:text-4xl font-bold mb-4">{{ $t('webDevelopmentInfo.responsiveIntegratedOptimized.description') }}</h1>
                 <p class="md:text-left md:w-full">
-                    Websites are the center piece for Inbound marketing. Websites must be built with careful consideration
-                    of
-                    SEO, social sharing, content strategy, marketing automation, dynamic personalization, lead scoring, and
-                    progessive profiling.
+                    {{ $t('webDevelopmentInfo.responsiveIntegratedOptimized.description') }}
                 </p>
             </div>
             <div>
@@ -82,16 +75,14 @@
 
         <div class="mt-20 pb-20 !-mx-6 p-20 bg-slate-900">
             <div class="flex justify-center">
-                <h1 class="text-2xl md:text-3xl xl:text-5xl !text-white font-Monda ">Why Every Business
-                    Needs A
-                    Website</h1>
+                <h1 class="text-2xl md:text-3xl xl:text-5xl !text-white font-Monda ">{{ $t('webDevelopmentInfo.whyEveyBusinessNeedsAWebsite.title') }}</h1>
             </div>
             <div class="grid grid-cols-1 md:grid-cols-2 gap-4 xl:px-20 mt-20">
                 <div class="flex flex-row items-start gap-4 mb-10 animated-container opacity-0" v-for="reason in reasons">
                     <img :src="reason.img" alt="">
                     <div class="flex flex-col gap-1 font-Monda">
-                        <h4 class="text-base md:text-xl !text-white font-bold">{{ reason.title }}</h4>
-                        <p class="text-xs md:text-sm">{{ reason.text }}
+                        <h4 class="text-base md:text-xl !text-white font-bold">{{ $t(reason.title) }}</h4>
+                        <p class="text-xs md:text-sm">{{ $t(reason.text) }}
                         </p>
                     </div>
                 </div>
@@ -153,32 +144,24 @@ export default {
 
         const reasons = ref([
             {
-                title: 'CREDIBILITY',
-                text: `One of the main reasons you should have a website for your business is to increase your organization's credibility. 
-                Chances are there are several providers offering a similar service to yours. One way you can stand out is by having a website 
-                that looks good and clearly communicates quality information to your consumers. Without a website, people may question your legitimacy as a business. Having a website is an opportunity to make a great first impression and give people comfort that you’re a real business. `,
+                title: 'webDevelopmentInfo.CREDIBILITY.title',
+                text: "webDevelopmentInfo.CREDIBILITY.text",
                 img: `https://thriveagency.com/files/web-design-brand-new.svg`
             },
             {
-                title: 'BRAND',
-                text: `Showcasing your brand to your prospective customers is one of the most important things that you can do. By clearly establishing 
-                who you are, what you represent and what you stand for, you increase the chances of your customers buying from you. This is also something that can set you apart from your competitors. Without a website, 
-                it can be incredibly challenging to do this because people can't easily find quality and reliable information on your business.`,
+                title: 'webDevelopmentInfo.BRAND.title',
+                text: "webDevelopmentInfo.BRAND.text",
                 img: `https://thriveagency.com/files/web-design-ranking-new.svg`
             },
             {
-                title: 'LEADS',
-                text: `Perhaps one of the most intriguing reasons to have a website for your business is because it can increase your chances of getting 
-                leads. Once people find you online, become interested in your product or service and want to know more, they'll know how to contact you thanks to the information on your website, which gives you the opportunity to increase your sales.
-                 Even though websites have a cost, when used correctly, they have a positive ROI.`,
+                title: 'webDevelopmentInfo.LEADS.title',
+                text: "webDevelopmentInfo.LEADS.text",
                 img: `https://thriveagency.com/files/web-design-leads-new.svg`
             },
             {
-                title: 'ORGANIC TRAFFIC',
-                text: `Once you're online and have an SEO-optimized website, you have a chance of showing up in Google search results. 
-                This means that when people are searching for a product or service, there is a chance your website will show up in the results.
-                 This gives you the opportunity to drastically increase your customer base. `,
-                img: `https://thriveagency.com/files/web-design-optimize-new.svg`
+                title: 'webDevelopmentInfo.ORGANIC_TRAFFIC.title',
+                text: "webDevelopmentInfo.ORGANIC_TRAFFIC.text",
+               img: `https://thriveagency.com/files/web-design-optimize-new.svg`
             }
         ])
 
