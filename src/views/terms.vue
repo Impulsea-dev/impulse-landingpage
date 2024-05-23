ff95cc87908bed4fbe3a03f33b783a565a48f785
dexstager <rxdraft@proton.me>
2024-05-23 13:36:41 -0600

    changes

--- src/views/terms.vue
+++ src/views/terms.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <div class="p-6 rounded-lg shadow-lg space-y-2">
+    <div class="p-6 rounded-lg shadow-lg space-y-2" v-if="$t('en')==='en'">
       <h1 class="text-3xl font-semibold mb-4 text-center">Terms of Service</h1>
       <p class="text-sm">These Terms of Service (or, "<strong>TOS</strong>") are part of the Contract (defined below)
         between you and Impulse.</p>
@@ -118,5 +118,101 @@
         </div>
       </section>
     </div>
+    <div class="p-6 rounded-lg shadow-lg space-y-2" v-else>
+      <h1 class="text-3xl font-semibold mb-4 text-center">Términos del Servicio</h1>
+      <p class="text-sm">Estos Términos del Servicio (o "<strong>TOS</strong>") son parte del Contrato (definido más abajo)
+        entre usted e Impulse.</p>
+      <p class="text-sm"><br />Estos TOS incorporan cualquier término adicional contenido en cualquier formulario de pedido en papel o en línea para
+        cualquier Servicio(s), así como cualquier folleto, guías de usuario, y cualquier otro material incorporado por referencia</p>
+
+      <section class="mb-2">
+        <h2 class="text-2xl font-semibold mt-4 mb-2">ACEPTACIÓN, INICIO Y CAMBIOS</h2>
+        <hr class="mb-2">
+        <div class="space-y-1">
+          <p class="text-sm"><strong><em>¿Cómo acepto el Contrato con Impulse y cuándo comienza el Contrato?</em></strong></p>
+          <p class="text-sm">El Contrato comienza y entra en vigencia una vez que ambas partes hayan firmado el contrato. Al aceptar
+            el Contrato, también está aceptando los términos y condiciones contenidos en cualquier Pedido/Material.
+            Su
+            uso continuado de un Servicio será su aceptación de tales términos y condiciones y estos TOS.</p>
+
+          <p><strong><em>¿Quién puede hacer cambios en el Contrato y cómo?</em></strong></p>
+          <p>Solo Impulse puede hacer cambios en el Contrato. Impulse puede cambiar el Contrato después de cualquier Período de Servicio Fijo
+            dando al menos 30 días de aviso previo al cambio por correo, fax, correo electrónico o poniéndolo a disposición
+            electrónicamente.
+            <strong>Quedará vinculado por las versiones revisadas de este Contrato que le enviemos</strong>. Su uso continuado
+            de
+            un Servicio será su aceptación de dicho(s) cambio(s).
+          </p>
+          <p><strong><em>¿Cuánto tiempo dura mi Contrato con Impulse? ¿Cómo se renueva?</em></strong><br />El término de cualquier
+            Servicio
+            comenzará en la Fecha de Inicio, y expirará al final del Período de Servicio Fijo, según corresponda
+            ("<strong>Período de Servicio Inicial</strong>"). Al expirar un Período de Servicio Inicial, el Servicio continuará
+            en un Período de Servicio Mensual a las tarifas entonces aplicables de Impulse a menos que cancele el Servicio
+            aplicable o a menos que Impulse o usted rescinda este Contrato.</p>
+          <p><strong><em>¿Cómo se termina el Contrato o algún Servicio?</em></strong><br />Puede terminar el Contrato enviando un correo electrónico a support@impulse.ky</p>
+          <p><strong><em>¿Existen tarifas por terminar anticipadamente?</em></strong><br />Sí. Debe pagar una tarifa por terminación anticipada
+            por cualquier Servicio(s) que termine antes del final de cualquier Período de Servicio Fijo. Las tarifas son iguales al
+            <strong>50%
+              del total de los cargos mensuales por cada Servicio terminado anticipadamente, multiplicado por el número de meses restantes en
+              el
+              Período de Servicio Fijo</strong>.
+          </p>
+          <p><strong><em>¿Puede Impulse suspender o terminar mis Servicio(s)?</em></strong><br />Impulse puede terminar o suspender inmediatamente o desconectar cualquier Servicio(s) por cualquiera de las siguientes razones: no pago de cualquier cantidad debida,
+            incumplimiento sustancial de este Contrato, o dirección de una autoridad legal.</p>
+        </div>
+      </section>
+
+      <section class="mb-4">
+        <h2 class="text-2xl font-semibold mt-4 mb-2">TARIFAS, PAGOS Y SANCIONES</h2>
+        <hr class="mb-2">
+        <p><strong><em>¿Cómo me factura Impulse? ¿Cuándo vencen los pagos?</em></strong><br />Debe pagar todas las cantidades adeudadas
+          <strong>en la fecha de vencimiento indicada en la factura</strong> ("<strong>Fecha de Vencimiento</strong>").
+        </p>
+      </section>
+
+      <section class="mb-4">
+        <h2 class="text-2xl font-semibold mt-4">REQUISITOS</h2>
+        <hr class="mb-2">
+        <p><em><strong>¿Puede Impulse requerir una verificación de crédito, referencias crediticias o un depósito de seguridad?
+            </strong></em><br />Sí.
+          Impulse puede requerir una verificación de crédito, referencias crediticias y/o un depósito de seguridad antes del inicio del proyecto. Usted
+          acepta que Impulse está autorizado a utilizar su información personalmente identificable en relación con dichas verificaciones de crédito y referencias.</p>
+
+      </section>
+      <section class="mb-4">
+        <h2 class="text-2xl font-semibold mt-4">INDEMNIZACIÓN</h2>
+        <hr class="mb-2">
+        <p><strong><em>¿En qué circunstancias debo indemnizar a Impulse?</em></strong><br />Usted acepta indemnizar,
+          defender y eximir de responsabilidad a Impulse por cualquier y todos los reclamos, demandas, acciones, costos, gastos, daños y
+          responsabilidades (incluidos honorarios y gastos legales razonables) ("<strong>Reclamaciones</strong>") que surjan de alguna manera
+          de
+          su uso y/o recepción de los Servicios y/o Equipos, incluidos pero no limitados a Reclamaciones de difamación e
+          infracción de propiedad intelectual. Su obligación bajo esta Sección se extiende a Impulse y a sus empresas afiliadas,
+          y a sus accionistas, directores, funcionarios, empleados, agentes y contratistas.</p>
+      </section>
+      <section class="mb-2">
+        <h2 class="text-2xl font-semibold mt-4">RESPONSABILIDAD DE IMPULSE</h2>
+        <hr class="mb-2">
+        <p>Impulse no es responsable de ningún daño sufrido en relación con este Contrato o por no firmarlo por usted o cualquier otra persona, incluidos los daños que sean directos, indirectos, consecuentes, incidentales, económicos, ejemplares o punitivos. Estos incluyen, pero no se limitan a, daños como pérdida de oportunidades comerciales o pérdida de datos. No importa si los daños se basan en negligencia, incumplimiento de contrato, uso fraudulento, pirateo de los servicios de Impulse, o cualquier otra razón. Además de las circunstancias descritas en la Sección anterior, Impulse no es responsable de ninguna pérdida, responsabilidad, daño, gasto o reclamo por cualquier falla del (los) Servicio(s) o Equipo, o por el cumplimiento por parte de Impulse de cualquier obligación relacionada con el (los) Servicio(s) y/o Equipo, debido a cualquier causa más allá de su control razonable (por ejemplo, actos de guerra, terrorismo, disturbios civiles, sabotaje, dificultades laborales, desastres naturales, cortes de energía, inundación, embargo, catástrofe inevitable, explosión, acto u omisión de una autoridad gubernamental, o acto u omisiones de un proveedor de servicios).</p>
+      </section>
+      <section>
+        <h2 class="text-2xl font-semibold mt-4">GENERAL</h2>
+        <hr class="mb-2">
+        <div class="space-y-1">
+          <p><strong><em>¿Qué leyes se aplican al Contrato? </em></strong><br />Las leyes de las Islas Caimán rigen.</p>
+          <p><strong><em>¿Puede usted y/o Impulse transferir este Contrato? </em></strong><br />Usted no puede transferir ni asignar
+            este Contrato, su cuenta, los Servicios sin el consentimiento previo por escrito de Impulse. Impulse puede transferir o
+            asignar
+            todo o parte de este Contrato (incluidos cualquier derecho en cuentas por cobrar) en cualquier momento sin previo aviso o
+            consentimiento.
+          </p>
+          <p><strong><em>¿Qué sucede si partes de este Contrato se vuelven inaplicables o Impulse no hace valer un derecho?
+              </em></strong><br />Si alguna parte de este Contrato se vuelve obsoleta, prohibida o inaplicable, los términos restantes seguirán aplicándose. Recuerde que incluso si Impulse decide no hacer valer alguna parte de este Contrato durante algún período de tiempo, el término aún sigue siendo válido y Impulse puede hacerlo valer en el futuro.</p>
+          <p></p>
+        </div>
+      </section>
+</div>
+
+
   </div>
 </template>
