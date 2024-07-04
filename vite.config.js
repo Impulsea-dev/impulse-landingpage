b832a464e313cfe0959f1a51483cea1c9d24f919
dexstager <ravtec123@gmail.com>
2024-07-04 16:10:16 -0600

    adding

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,8 @@ import basicSsl from '@vitejs/plugin-basic-ssl'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  assetsInclude: ['**/*.mov'],
+
   plugins: [
     Vue(),
     ViteImages({
