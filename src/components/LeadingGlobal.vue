c55298d8533610ad23384afd82e69a850582a974
David Alvarado <davidalvarado470@gmail.com>
2024-07-08 09:44:43 -0600

    changes

--- src/components/LeadingGlobal.vue
+++ src/components/LeadingGlobal.vue
@@ -8,7 +8,9 @@
             <article class="bg-[#F6F6F6] border shadow-2xl mx-5 md:mx-10 xl:mx-20 my-10 md:my-40 overflow-hidden">
                 <div class="flex flex-col md:flex-row justify-between items-center px-5 md:px-16 py-10 md:py-20">
                     <div class="flex flex-col px-5">
-                        <span class="relative text-center px-6 py-3 cursor-pointer border text-base md:text-lg xl:text-2xl font-bold" :class="{
+                        <span
+                            class="relative text-center px-6 py-3 cursor-pointer border text-base md:text-lg xl:text-2xl font-bold"
+                            :class="{
                                 'bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 text-white scale-x-110': currentBeni == i,
                                 'bg-[#F2EDED] text-gray-400': currentBeni != i,
 
@@ -16,11 +18,12 @@
                             {{ beni.title }}
                         </span>
                     </div>
-                    <img src="../assets/images/arrows.svg" class="animate-pulse h-20 xl:h-40" alt="">
-                    <p class="text-base xl:text-xl text-[#263238] font-bold md:w-[377px] text-center md:text-start">
+                    <img src="../assets/images/arrows.svg" class="hidden md:block animate-pulse h-20 xl:h-40" alt="">
+                    <p class="text-base xl:text-xl text-[#263238] font-bold md:w-[377px] text-center md:text-start mt-4 md:mt-0">
                         {{ benifits[currentBeni].subtitle }}
                     </p>
-                    <img :src="benifits[currentBeni].img" class="h-60 md:h-40 xl:h-80 hover:scale-95 transition-transform duration-300" alt="">
+                    <img :src="benifits[currentBeni].img"
+                        class="h-60 md:h-40 xl:h-80 hover:scale-95 transition-transform duration-300" alt="">
                 </div>
             </article>
             <img src="../assets/images/rectangle.svg" class="hidden md:block absolute -top-44 left-0 -z-10" alt="">
