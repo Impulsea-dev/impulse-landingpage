6fbdc25d0396ddf55901845e015dc142b2b95232
David Alvarado <davidalvarado470@gmail.com>
2024-10-22 11:17:56 -0600

    updated vision and mission

--- src/components/aboutus/MissionVision.vue
+++ src/components/aboutus/MissionVision.vue
@@ -10,16 +10,20 @@ import vision from "@/assets/images/vision.svg"
 const items = [
     {
         title: 'Our Mission',
-        subtitle:'The telecommunications industry holds vast untapped potential.',
-        subtitledes: `Our aim is to assist companies in increasing their profitability through our solutions and services. We are dedicated to creating most innovative solutions for the industry and achieving this by attentively listening to our customers and being their most reliable partners.`,
+        subtitle: 'The telecommunications industry holds vast untapped potential.',
+        subtitledes: `At Impulse, our mission is to empower small and medium Internet Service Providers (ISPs) with the 
+        cutting-edge technology they need to thrive. By enhancing their operational efficiency and profitability, 
+        we help these ISPs connect more people, sustain their businesses, and contribute to a decentralized internet 
+        that benefits all`,
         img: mission
 
     },
     {
         title: 'Our Vision',
-        subtitle:'Our vision is to be the leading provider of telecom solutions worldwide.',
-        subtitledes: `We are committed to developing most innovative technology, tools, and services for our partners, and are dedicated to making this vision a reality. We are constantly seeking out new talent and innovative technologies to help us achieve our goal, and 
-        our partners will benefit from our relentless pursuit of excellence.`,
+        subtitle: 'Our vision is to be the leading provider of telecom solutions worldwide.',
+        subtitledes: `To create a future where the internet remains a decentralized space, empowering communities, 
+        fostering innovation, and ensuring equal access to opportunities for everyone, 
+        regardless of their location or circumstances.`,
         img: vision
     }]
 </script>
