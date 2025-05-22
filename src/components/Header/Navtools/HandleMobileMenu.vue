0e43f341a2966bbc2a488747f8e92f6e8ed96a6e
David Alvarado <d.alvarado@impulse.ky>
2025-05-22 11:55:52 -0600

    changes

--- src/components/Header/Navtools/HandleMobileMenu.vue
+++ src/components/Header/Navtools/HandleMobileMenu.vue
@@ -1,12 +1,17 @@
 <template>
-  <div
+<div class="flex gap-4">
+    <Language />
+    <div
     class="cursor-pointer text-current text-2xl"
     @click="toggleMsidebar"
   >
     <Icon icon="material-symbols:menu-rounded" />
   </div>
+</div>  
+
 </template>
 <script setup>
+import Language from "./Language.vue";
 import Icon from "@/components/Icon";
 import { useThemeSettingsStore } from "@/store/themeSettings";
 
